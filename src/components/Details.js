e264a523a9f9ffdea6ad45d8789b0d42721d8e17
Nikhil Hira <nikhil@leancode.io>
2019-01-31 15:51:37 -0800

    1. add react-router-dom 2. import BrowserRouter, Switch, Route 3. add links to navbar component

--- src/components/Details.js
+++ src/components/Details.js
@@ -4,7 +4,7 @@ export default class Details extends Component {
   render() {
     return (
       <div>
-        <h3>Hello from De</h3>
+        <h3>Hello from Details</h3>
       </div>
     );
   }
--- src/components/Product.js
+++ src/components/Product.js
@@ -1,11 +1,13 @@
-import React, { Component } from 'react';
+import React from 'react';
 
-export default class Product extends Component {
+export default class Product extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
   render() {
     return (
-      <div>
-        <h3>Hello from Product</h3>
-      </div>
+      <div> <h3>Hello from Product</h3></div>
     );
   }
 }
