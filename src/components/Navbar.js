0fbd9878e2b0d5743b72efcb2e5b8f1d1410f29f
Nikhil Hira <nikhil@leancode.io>
2019-02-05 18:15:06 -0800

    add styled-components and create ButtonContainer and NavWrapper components

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react'
 import { Link } from 'react-router-dom';
 import logo from '../logo.svg';
+import styled from 'styled-components';
+import {ButtonContainer} from './Button';
 export default class Navbar extends Component {
   render() {
     return (
       <div>
-        <nav className="navbar navbar-expand-sm navbar-dark px-sm-5">
+        <NavWrapper className="navbar navbar-expand-sm navbar-dark px-sm-5">
           {/* 
             https:   //www.iconfinder.com/icons/1243689/call_phone_icon
             Creative Commons (Attribution 3.0 Unported);
@@ -15,16 +17,27 @@ export default class Navbar extends Component {
           </Link>
           <ul className='navbar-nav align-items-center'>
             <li className='nav-item ml-5'>
-              <Link to="/">Products</Link>
+              <Link to="/" className='nav-link'>products</Link>
             </li>
           </ul>
         <Link to='/cart' className='ml-auto'>
-            <button>
-              <i className='fas fa-cart-plus'> My Cart</i>
-            </button>
+            <ButtonContainer>
+              <span className='mr-2'>
+                <i className='fas fa-cart-plus'> My Cart</i>
+              </span>
+            </ButtonContainer>
         </Link>
-        </nav>
+        </NavWrapper>
       </div>
     )
   }
 }
+
+const NavWrapper = styled.nav`
+background: var(--mainBlue);
+.nav-link {
+  color         : var(--mainWhite) !important;
+  font-size     : 1.3rem;
+  text-transform: capitalize;
+}
+`
