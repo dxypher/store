6055162569d05d357ab3c977c13486853f393817
Nikhil Hira <nikhil@leancode.io>
2019-02-07 18:00:55 -0800

    add title component

--- src/components/ProductList.js
+++ src/components/ProductList.js
@@ -1,12 +1,20 @@
 import React, { Component } from 'react'
 import Product from './Product'
+import Title from './Title';
 
 export default class ProductList extends Component {
+  state = {
+    products: []
+  }
   render() {
     return (
-      <div>
-        <Product/>
-      </div>
+      // <Product/>
+      <React.Fragment>  
+        <div className='py-5'></div>
+        <div className='container'></div>
+        <Title name='our' title='products'/>
+        <div className='row'></div>
+      </React.Fragment>
     )
   }
 }
