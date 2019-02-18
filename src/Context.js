445f9313d4236b3ae5cd8d59f5c82eff68e603b0
Nikhil Hira <nikhil@leancode.io>
2019-02-17 16:42:43 -0800

    start creating cart components, CartColumns

--- src/Context.js
+++ src/Context.js
@@ -10,7 +10,10 @@ class ProductProvider extends Component {
     detailProduct,
     cart        : [],
     modalOpen   : false,
-    modalProduct: detailProduct
+    modalProduct: detailProduct,
+    cartSubTotal: 0,
+    cartText    : 0,
+    cartTotal   : 0
   };
 
   componentDidMount() {
@@ -76,6 +79,22 @@ class ProductProvider extends Component {
     })
   }
 
+  increment = (id) => {
+    console.log('this is the increment method');
+  }
+
+  decrement = (id) => {
+    console.log('this is the decrement method');
+  }
+
+  removeItem = (id) => {
+    console.log('this is removeItem method');
+  }
+
+  clearCart = () => {
+    console.log('cart cleared');
+  }
+
   render() {
     return (
       <ProductContext.Provider 
@@ -84,7 +103,11 @@ class ProductProvider extends Component {
         handleDetail: this.handleDetail,
         addToCart   : this.addToCart,
         openModal   : this.openModal,
-        closeModal  : this.closeModal
+        closeModal  : this.closeModal,
+        increment   : this.increment,
+        decrement   : this.decrement,
+        removeItem  : this.removeItem,
+        clearCart   : this.clearCart
       }}>
         {this.props.children}
       </ProductContext.Provider>
