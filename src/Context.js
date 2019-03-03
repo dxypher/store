9a25b5668eb96572a140a68032318e92aec38842
Nikhil Hira <nikhil@leancode.io>
2019-03-03 11:19:35 -0800

    implement decrement

--- src/Context.js
+++ src/Context.js
@@ -96,19 +96,37 @@ class ProductProvider extends Component {
       return {
         cart: [...tempCart]
       }
-    }, () => {this.addTotals()})
+    }, () => {this.addTotals();
+    })
   }
 
   decrement = (id) => {
-    console.log('this is the decrement method');
+    let   tempCart        = [...this.state.cart];
+    const selectedProduct = tempCart.find(item => item.id === id);
+    const index           = tempCart.indexOf(selectedProduct);
+    const product         = tempCart[index];
+
+    product.count -= 1;
+
+    if (product.count === 0) {
+      this.removeItem(id);
+    }
+    else {
+      product.total = product.count * product.price;
+    }
+
+    this.setState(() => {
+      return {
+        cart: [...tempCart]
+      }
+    }, () => { this.addTotals();
+    })
   }
 
   removeItem = (id) => {
-    let tempProducts = [...this.state.products];
-    let tempCart     = [...this.state.cart];
-
-    tempCart = tempCart.filter(item => item.id != id);
-
+    let   tempProducts          = [...this.state.products];
+    let   tempCart              = [...this.state.cart];
+          tempCart              = tempCart.filter(item => item.id !== id);
     const index                 = tempProducts.indexOf(this.getItem(id));
     let   removedProduct        = tempProducts[index];
           removedProduct.inCart = false;
