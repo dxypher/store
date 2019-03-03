67b377de967bdf0f7abc96443c183b389f607937
Nikhil Hira <nikhil@leancode.io>
2019-03-03 10:31:20 -0800

    implement clearCart

--- src/Context.js
+++ src/Context.js
@@ -96,7 +96,12 @@ class ProductProvider extends Component {
   }
 
   clearCart = () => {
-    console.log('cart cleared');
+    this.setState(() => {
+      return {cart: []}
+    }, () => {
+      this.setProducts();
+      this.addTotals();
+    });
   }
 
   addTotals = () => {
